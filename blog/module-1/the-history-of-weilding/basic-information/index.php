75064d7fc7c86342f01f8d51dee8aafc28548e3f
gruntik <gruntov99@mail.ru>
2021-12-24 14:00:31 +0300

    Added What do welders do without vocabulary

--- blog/module-1/the-history-of-weilding/basic-information/index.php
+++ blog/module-1/the-history-of-weilding/basic-information/index.php
@@ -52,22 +52,22 @@
 
         <div>
           <div class="p-main">
-            <span class="p-first-words">In the 19th century</span>, main breakthroughs were made in welding. Scientists and engineers from the UK and the USA took an active part in advancing welding technologies.
+            <span class="p-first-words-bold">In the 19th century</span>, main breakthroughs were made in welding. Scientists and engineers from the UK and the USA took an active part in advancing welding technologies.
           </div>
           <div class="p-main">
-            <span class="p-first-words">Sir Humphry Davy</span>, a famous British chemist and inventor, produced an arc between two carbon electrodes using a battery in 1800. His cousin, Edmund Davy, discovered acetylene in 1836.
+            <span class="p-first-words-bold">Sir Humphry Davy</span>, a famous British chemist and inventor, produced an arc between two carbon electrodes using a battery in 1800. His cousin, Edmund Davy, discovered acetylene in 1836.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In 1890 Charles L.Coffin</span>, an American inventor, got the first US patent for an arch welding process using a metal electrode.
+            <span class="p-first-words-bold">In 1890 Charles L.Coffin</span>, an American inventor, got the first US patent for an arch welding process using a metal electrode.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In 1900</span> Arthur Percy Strohmenger introduced a coated metal electrode in Great Britain. There was a thin coating of clay or lime, but it provided a more stable arc.
+            <span class="p-first-words-bold">In 1900</span> Arthur Percy Strohmenger introduced a coated metal electrode in Great Britain. There was a thin coating of clay or lime, but it provided a more stable arc.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In about 1900</span>, a torch suitable for low-pressure acetylene was invented.
+            <span class="p-first-words-bold">In about 1900</span>, a torch suitable for low-pressure acetylene was invented.
           </div>
           <div class="p-main">
-            <span class="p-first-words">The First World War</span> made the countries develop weapons production and welding was in high demand. In America and in Europe many companies appeared to produce welding machines and electrodes.
+            <span class="p-first-words-bold">The First World War</span> made the countries develop weapons production and welding was in high demand. In America and in Europe many companies appeared to produce welding machines and electrodes.
           </div>
         </div>
         </div>
@@ -78,19 +78,19 @@
 
         <div>
           <div class="p-main">
-            <span class="p-first-words">In 1919</span> The American Welding Society as a non-profit organization was founded. The aim of the society was to advance welding and allied processes.
+            <span class="p-first-words-bold">In 1919</span> The American Welding Society as a non-profit organization was founded. The aim of the society was to advance welding and allied processes.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In 1920</span> automatic welding was introduced by The General Electric Company. It was used to build up motor shafts and cranes. It was also used by the automobile industry to produce rear axles.
+            <span class="p-first-words-bold">In 1920</span> automatic welding was introduced by The General Electric Company. It was used to build up motor shafts and cranes. It was also used by the automobile industry to produce rear axles.
           </div>
           <div class="p-main">
-            <span class="p-first-words">During the 1920s</span> various types of welding electrodes were developed.
+            <span class="p-first-words-bold">During the 1920s</span> various types of welding electrodes were developed.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In 1927</span> the heavy-coated electrodes were introduced by the A.O.Smith Company in America. The electrodes were made by extruding.
+            <span class="p-first-words-bold">In 1927</span> the heavy-coated electrodes were introduced by the A.O.Smith Company in America. The electrodes were made by extruding.
           </div>
           <div class="p-main">
-            <span class="p-first-words">In 1929</span> the Lincoln Electric Company produced extruded electrode rods.
+            <span class="p-first-words-bold">In 1929</span> the Lincoln Electric Company produced extruded electrode rods.
           </div>
         </div>
 
@@ -101,7 +101,7 @@
           
           <div>
             <div class="p-main">
-              <span class="p-first-words">In 1930</span> covered electrodes were widely used.
+              <span class="p-first-words-bold">In 1930</span> covered electrodes were widely used.
             </div>
             <div class="p-main">
               The contribution of British and American inventors in the development of welding was great.
@@ -113,9 +113,10 @@
        
       </div> <!-- col-md-8 -->
 
-      </div>
+      </div> <!--? -->
 
-      </div>
+      </div> <!--? -->
+
     </section>
   
   </main>
