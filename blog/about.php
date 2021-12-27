b363c681cb952e8191397c94687cf8bd616cbf72
gruntik <gruntov99@mail.ru>
2021-12-27 16:58:37 +0300

    Added image on index and added about page

---
+++ blog/about.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>English for welders</title>
+  <?php include('fragments/links-style.php') ?>
+  <link href="css/blog.css" rel="stylesheet">
+  <link href="css/style-main.css" rel="stylesheet">
+  <link href="css/style-basic.css" rel="stylesheet">
+  <link href="css/sidebars.css" rel="stylesheet">
+  <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <script src="js/sidebars.js"></script>
+
+  <style>
+    .font-about {
+      font-size: 1.25rem;
+      font-weight: 350;
+    }
+  </style>
+
+</head>
+
+<body>
+  <div class="container">
+    <?php include('fragments/header.php'); ?>
+  </div>
+
+  <main class="container container2">
+
+    <section class="home-section ">
+
+      <div class="row">
+
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+
+          <div class="py-1 text-center container ">
+            <h2 class="fw-light">The main information</h2>
+          </div>
+
+          <div class="mb-5">
+
+            <div class="p-main font-about">
+              <span class="p-first-words" />
+              Personal site for the students of welding careers as a support in studying the English language as a professional component
+            </div>
+
+            <div class="p-main font-about">
+              <span class="p-first-words" />
+              Электронный образовательный ресурс для учащихся специальности 2-36 01 06-01 «Оборудование и технология сварочного производства» как инструмент для изучения учебной дисциплины «Иностранный язык (профессиональная лексика)»
+            </div>
+
+            <div class="text-center mb-2">
+              <img class="text-center img-basic" src="_img/about1.jpg">
+            </div>
+            <div class="text-center mb-2">
+              <img class="text-center img-basic" src="_img/about2.jpg">
+            </div>
+
+          </div> <!-- mb-5 -->
+
+        </div> <!-- col-md-10 -->
+        <div class="col-md-1"></div>
+
+      </div> <!-- row -->
+
+    </section>
+
+  </main>
+
+  <?php include('fragments/footer.php') ?>
+
+</body>
+
+</html>
--- blog/index.php
+++ blog/index.php
@@ -1,8 +1,9 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>English for welders</title>
-    <?php include('fragments/links-style.php') ?>
+
+<head>
+  <title>English for welders</title>
+  <?php include('fragments/links-style.php') ?>
   <link href="css/blog.css" rel="stylesheet">
   <link href="css/style-main.css" rel="stylesheet">
   <link href="css/sidebars.css" rel="stylesheet">
@@ -10,53 +11,67 @@
 
   <script src="js/sidebars.js"></script>
   <!-- <script src="assets/dist/js/bootstrap.bundle.min.js"></script> top menu -->
-  </head>
-  <body>
+  <style>
+    .the-main-information {
+      background-image: url('_img/the-main-information.jpg');
+    }
 
-<div class="container">
-  <?php include ('fragments/header.php');?>
-  
-  <nav class="navbar navbar-expand-sm navbar-light " aria-label="Third navbar example" hidden>
-    <div class="container-fluid">
-      <a class="navbar-brand link-secondary" href="#">Expand at sm</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    .shadow-for-white-text {
+      text-shadow: 2px 1px 2px black;
+    }
 
-      <div class="collapse navbar-collapse" id="navbarsExample03">
-        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-          <li class="nav-item">
-            <a class="nav-link active link-secondary" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle link-secondary" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown03">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
+    .fit-image-div {
+      object-fit: cover;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="container">
+    <?php include('fragments/header.php'); ?>
+
+    <nav class="navbar navbar-expand-sm navbar-light " aria-label="Third navbar example" hidden>
+      <div class="container-fluid">
+        <a class="navbar-brand link-secondary" href="#">Expand at sm</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+          <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+            <li class="nav-item">
+              <a class="nav-link active link-secondary" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link-secondary" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle link-secondary" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-</div>
-
-<main class="container container2 mt-2">
-  <div class="p-4  mb-4 text-white rounded bg-dark" >
-    <div class="col-md-8 px-0">
-      <h1 class="display-6 fst-italic">The main information about our site</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
-    </div>
   </div>
 
-  <div class="d-flex flex-row flex-nowrap overflow-auto scrollbar-div pb-1">
-      <div class="card card-block mr-2" style="min-width: 45%;">
+  <main class="container container2 mt-2">
+    <div class="p-4 mb-4 text-white rounded bg-dark the-main-information">
+      <div class="col-md-8 px-0">
+        <h1 class="display-6 fst-italic shadow-for-white-text">The main information</h1>
+        <p class="lead my-3 shadow-for-white-text">Personal site for the students of welding careers as a support in studying the English language as a professional component</p>
+        <p class="lead mb-0"><a href="about.php" class="text-white fw-bold">Continue reading...</a></p>
+      </div>
+    </div>
+
+    <div class="d-flex flex-row flex-nowrap overflow-auto scrollbar-div pb-1">
+      <div class="card card-block mr-2 me-2" style="min-width: 45%;">
         <div class="row g-0 border rounded overflow-hidden flex-md-row shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-primary">About</strong>
@@ -64,15 +79,12 @@
             <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
             <a href="module-1/index.php" class="stretched-link">Continue reading</a>
           </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-             role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-             <title>Placeholder</title>
-             <rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <div class="col-auto d-none d-lg-block" style="background: red">
+            <img class="bd-placeholder-img fit-image-div" width="200" height="250" src="_img/module1.jpg">
           </div>
         </div>
       </div>
-      <div class="card card-block mr-2" style="min-width: 45%;">
+      <div class="card card-block mr-2 me-2" style="min-width: 45%;">
         <div class="row g-0 border rounded overflow-hidden flex-md-row shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-success">Mechanical</strong>
@@ -81,14 +93,11 @@
             <a href="#" class="stretched-link">Continue reading</a>
           </div>
           <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-             role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-             <title>Placeholder</title>
-             <rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="bd-placeholder-img fit-image-div" width="200" height="250" src="_img/module2.jpg">
           </div>
         </div>
       </div>
-      <div class="card card-block mr-2" style="min-width: 45%;">
+      <div class="card card-block mr-2 me-2" style="min-width: 45%;">
         <div class="row g-0 border rounded overflow-hidden flex-md-row shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-primary">About</strong>
@@ -97,10 +106,7 @@
             <a href="#" class="stretched-link">Continue reading</a>
           </div>
           <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-             role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-             <title>Placeholder</title>
-             <rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="bd-placeholder-img fit-image-div" width="200" height="250" src="_img/module3.jpg">
           </div>
         </div>
       </div>
@@ -113,16 +119,14 @@
             <a href="#" class="stretched-link">Continue reading</a>
           </div>
           <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-             role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-             <title>Placeholder</title>
-             <rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img class="bd-placeholder-img fit-image-div" width="200" height="250" src="_img/module4.jpg">
           </div>
         </div>
       </div>
- </div>
+    </div>
 
-</main>
+  </main>
   <?php include('fragments/footer.php') ?>
-  </body>
+</body>
+
 </html>
