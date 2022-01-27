408221084670a4caff9493fb3e0489d17e518e89
gruntik <gruntov99@mail.ru>
2022-01-27 11:05:54 +0300

    edit footer

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,8 +1,7 @@
 <footer class="blog-footer footer container">
-  <p>English for welders for <a href="https://vgtk.by/" target="_blank">VGTK</a>.</p>
+  <p>English for welders by <a href="https://vgtk.by/" target="_blank">VGTK</a>.</p>
   <p>
     <a href="#">Back to top</a>
   </p>
 
-  <!-- <script src="assets/dist/js/bootstrap.bundle.min.js"></script> -->
 </footer>
